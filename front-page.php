72b2a8afef0fab79a46ef52591e6788d0be050c7
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2019-10-19 13:48:11 +0300

    add slider section file

--- front-page.php
+++ front-page.php
@@ -3,34 +3,9 @@
  *Template Name: Home Page
  */
 get_header(); ?>
-<section class="home-slider owl-carousel">
-	  <div class="slider-item" style="background-image:url(<?php echo get_theme_file_uri('/images/bg_1.jpg') ?>);" data-stellar-background-ratio="0.5">
-      	<div class="overlay"></div>
-        <div class="container">
-          <div class="row no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
-          <div class="col-md-6 text ftco-animate pl-md-5">
-            <h1 class="mb-4">Base Construction <span>Build The Future</span></h1>
-            <h3 class="subheading">A small river named Duden flows by their place and supplies it with the necessary regelialia</h3>
-            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Request A Quote</a></p>
-          </div>
-        </div>
-        </div>
-      </div>
 
-      <div class="slider-item" style="background-image:url(<?php echo get_theme_file_uri('/images/bg_2.jpg') ?>);" data-stellar-background-ratio="0.5">
-      	<div class="overlay"></div>
-        <div class="container">
-          <div class="row no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
-          <div class="col-md-6 text ftco-animate pl-md-5">
-            <h1 class="mb-4">We Turn Your <span>Vision Into Reality</span></h1>
-            <h3 class="subheading">A small river named Duden flows by their place and supplies it with the necessary regelialia</h3>
-            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Request A Quote</a></p>
-          </div>
-        </div>
-        </div>
-      </div>
-    </section>
-	
+<?php 
+get_template_part( 'template-parts/section-slider');	
 get_template_part( 'template-parts/section-about');
 get_template_part( 'template-parts/section-services');
 get_template_part( 'template-parts/section-intro');
@@ -38,6 +13,6 @@ get_template_part( 'template-parts/section-team');
 get_template_part( 'template-parts/section-progect');
 get_template_part( 'template-parts/section-testimonials');
 get_template_part( 'template-parts/section-blog');
-
+?>
 
 <?php get_footer(); ?>		
---
+++ template-parts/section-slider.php
@@ -0,0 +1,37 @@
+<?php 
+
+/**
+ * The template for displaying the Slider hero section
+ *
+ * @package bee
+ */
+
+?>
+
+<section class="home-slider owl-carousel">
+	  <div class="slider-item" style="background-image:url(<?php echo get_theme_file_uri('/images/bg_1.jpg') ?>);" data-stellar-background-ratio="0.5">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
+          <div class="col-md-6 text ftco-animate pl-md-5">
+            <h1 class="mb-4">Base Construction <span>Build The Future</span></h1>
+            <h3 class="subheading">A small river named Duden flows by their place and supplies it with the necessary regelialia</h3>
+            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Request A Quote</a></p>
+          </div>
+        </div>
+        </div>
+      </div>
+
+      <div class="slider-item" style="background-image:url(<?php echo get_theme_file_uri('/images/bg_2.jpg') ?>);" data-stellar-background-ratio="0.5">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
+          <div class="col-md-6 text ftco-animate pl-md-5">
+            <h1 class="mb-4">We Turn Your <span>Vision Into Reality</span></h1>
+            <h3 class="subheading">A small river named Duden flows by their place and supplies it with the necessary regelialia</h3>
+            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">Request A Quote</a></p>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
