fdf589480aec595a2dbfbf5d671e1326332f891b
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2019-11-02 15:24:57 +0200

    add coment section into the comment.php

--- comments.php
+++ comments.php
@@ -15,6 +15,48 @@
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
+
+<li class="comment">
+<div class="vcard bio">
+  <img src="images/person_1.jpg" alt="Image placeholder">
+</div>
+<div class="comment-body">
+  <h3>John Doe</h3>
+  <div class="meta mb-2">February 07, 2019 at 2:21pm</div>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
+  <p><a href="#" class="reply">Reply</a></p>
+</div>
+</li>
+</ul>
+<div class="comment-form-wrap pt-5">
+<h3 class="mb-5 h4 font-weight-bold">Leave a comment</h3>
+<form action="#" class="p-5 bg-light">
+  <div class="form-group">
+	<label for="name">Name *</label>
+	<input type="text" class="form-control" id="name">
+  </div>
+  <div class="form-group">
+	<label for="email">Email *</label>
+	<input type="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+	<label for="website">Website</label>
+	<input type="url" class="form-control" id="website">
+  </div>
+
+  <div class="form-group">
+	<label for="message">Message</label>
+	<textarea name="" id="message" cols="30" rows="10" class="form-control"></textarea>
+  </div>
+  <div class="form-group">
+	<input type="submit" value="Post Comment" class="btn py-3 px-4 btn-primary">
+  </div>
+
+</form>
+</div>
+</div>
+</div> <!-- .col-md-8 -->
+
 if ( post_password_required() ) {
 	return;
 }
