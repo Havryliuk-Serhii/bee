6edaa5ac302e318c02e776c8682305ae89016dcb
Serhii Havryliuk <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-03-31 14:41:57 +0300

    finished intro section

--- template-parts/sections/section-intro.php
+++ template-parts/sections/section-intro.php
@@ -4,3 +4,24 @@
  * @package bee
  */
 ?>
+<?php
+    $intro_post = new WP_Query(array('category_name' => 'intro','order' => 'ASC'));
+    if ($intro_post->have_posts() ) :
+    while ($intro_post->have_posts() ) : $intro_post->the_post(); 
+?>
+<section class="ftco-intro" <?php echo bee_thumbnail(); ?> data-stellar-background-ratio="0.5">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-9 text-center">
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_excerpt(); ?>
+                    <p class="mb-0"><a href="<?php the_permalink(); ?>" class="btn btn-primary px-4 py-3"><?php esc_html_e('Know more about us','bee' ); ?></a></p>
+                </div>
+            </div>
+        </div>
+</section>
+<?php endwhile;
+        endif;
+    wp_reset_query();
+?>
