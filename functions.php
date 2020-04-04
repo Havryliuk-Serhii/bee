d07a029f77cb5baabf19940c0f79e5d4ef64a040
Serhii Havryliuk <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-04-04 14:27:54 +0300

    add pagination at index.php

--- functions.php
+++ functions.php
@@ -314,7 +314,79 @@ function testimonial_posts(){
 	);
 	register_post_type('testimonial_slider', $args);
 }
+/**
+*  Pagination
+**/
+function bee_pagination( $args = array() ) {
 
+    $defaults = array(
+        'range'           => 3,
+        'custom_query'    => false,
+        'previous_string' => __( '<', 'bee' ),
+        'next_string'     => __( '>', 'bee' ),
+        'before_output'   => '<ul>',
+        'after_output'    => '</ul>'
+    );
+
+    $args = wp_parse_args(
+        $args,
+        apply_filters( 'bee_pagination_defaults', $defaults )
+    );
+
+    $args['range'] = (int) $args['range'] - 1;
+    if ( !$args['custom_query'] )
+        $args['custom_query'] = @$GLOBALS['wp_query'];
+    $count = (int) $args['custom_query']->max_num_pages;
+    $page  = intval( get_query_var( 'paged' ) );
+    $ceil  = ceil( $args['range'] / 2 );
+
+    if ( $count <= 1 )
+        return FALSE;
+
+    if ( !$page )
+        $page = 1;
+
+    if ( $count > $args['range'] ) {
+        if ( $page <= $args['range'] ) {
+            $min = 1;
+            $max = $args['range'] + 1;
+        } elseif ( $page >= ($count - $ceil) ) {
+            $min = $count - $args['range'];
+            $max = $count;
+        } elseif ( $page >= $args['range'] && $page < ($count - $ceil) ) {
+            $min = $page - $ceil;
+            $max = $page + $ceil;
+        }
+    } else {
+        $min = 1;
+        $max = $count;
+    }
+
+    $echo = '';
+    $previous = intval($page) - 1;
+    $previous = esc_attr( get_pagenum_link($previous) );
+
+    if ( $previous && (1 != $page) )
+        $echo .= '<li><a aria-label="Previous" href="' . $previous . '" title="' . __( 'previous', 'bee') . '"><i class="ion-ios-arrow-back">' . $args['previous_string'] . '</i></a></li>';
+
+    if ( !empty($min) && !empty($max) ) {
+        for( $i = $min; $i <= $max; $i++ ) {
+            if ($page == $i) {
+                $echo .= '<li class="active" aria-current="page"><span>' . str_pad( (int)$i, 1, '0', STR_PAD_LEFT ) . '</span></li>';
+            } else {
+                $echo .= sprintf( '<li aria-current="page"><a href="%s">%2d</a></li>', esc_attr( get_pagenum_link($i) ), $i );
+            }
+        }
+    }
+
+    $next = intval($page) + 1;
+    $next = esc_attr( get_pagenum_link($next) );
+    if ($next && ($count != $page) )
+        $echo .= '<li><a aria-label="Next" href="' . $next . '" title="' . __( 'next', 'zlv') . '"><i class="ion-ios-arrow-forward">' . $args['next_string'] . '</i></a></li>';
+
+    if ( isset($echo) )
+        echo $args['before_output'] . $echo . $args['after_output'];
+}
 /**
 * Social icon links
 **/
