e1915c3888a701c6d161c9727a938b60ed4c6433
Serhii Havryliuk <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-04-06 23:23:45 +0300

    add tag into the single.php

--- single.php
+++ single.php
@@ -15,18 +15,10 @@
                 <?php the_content(); ?>
                 <div class="tag-widget post-tag-container mb-5 mt-5">
                     <div class="tagcloud">
-
-                    <!--
-                        <a href="#" class="tag-cloud-link">Life</a>
-                        <a href="#" class="tag-cloud-link">Sport</a>
-                        <a href="#" class="tag-cloud-link">Tech</a>
-                        <a href="#" class="tag-cloud-link">Travel</a>
-                    -->
+                        <?php the_tags('', '',''); ?>
                     </div>
                 </div>
                 <?php echo bee_author(); ?>
-
-
             </div>
             <?php endwhile; ?>
             <?php get_sidebar(); ?>
