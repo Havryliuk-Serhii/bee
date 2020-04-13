702fe72873be73815c08083be2d5b9855774f6fd
Serhii Havryliuk <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-04-14 00:38:11 +0300

    add archive section at sidebar

--- sidebar.php
+++ sidebar.php
@@ -3,8 +3,6 @@
  * The sidebar containing the main widget area
  * @package bee
  */
-
-
 ?>
 <div class="col-lg-4 sidebar ftco-animate">
   	<div class="sidebar-box">
@@ -18,11 +16,7 @@
   	<div class="sidebar-box ftco-animate">
 	  	<h3><?php esc_html_e('Category','bee' ); ?></h3>
 		<ul class="categories">
-	  		<li><a href="#">Construction <span>(6)</span></a></li>
-	  		<li><a href="#">House Renovation <span>(8)</span></a></li>
-	  		<li><a href="#">Painting <span>(2)</span></a></li>
-	  		<li><a href="#">Architecture Design <span>(2)</span></a></li>
-	  		<li><a href="#">Re-construction <span>(2)</span></a></li>
+
 		</ul>
   	</div>
   	<div class="sidebar-box ftco-animate">
@@ -38,10 +32,7 @@
   	<div class="sidebar-box ftco-animate">
 	  	<h3><?php esc_html_e('Archives','bee' ); ?></h3>
 		<ul class="categories">
-			<li><a href="#">December 2018 <span>(30)</span></a></li>
-			<li><a href="#">Novemmber 2018 <span>(20)</span></a></li>
-			<li><a href="#">September 2018 <span>(6)</span></a></li>
-			<li><a href="#">August 2018 <span>(8)</span></a></li>
+			<?php wp_get_archives('type=monthly&show_post_count=1'); ?>
 		</ul>
   	</div>
 	<div class="sidebar-box ftco-animate">
